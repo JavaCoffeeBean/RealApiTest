80fcacb922bd796d7aea2add3501e96231f56dc9
JavaCoffeeBean <JavaCoffeeBean@gmail.com>
2020-01-09 16:43:34 -0600

    converted from retrofit to volley

--- app/src/main/java/com/example/android/realapitest/MainActivity.java
+++ app/src/main/java/com/example/android/realapitest/MainActivity.java
@@ -5,77 +5,80 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 import java.math.BigInteger;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
     private Object pop;
     private String pop2;
+    private RequestQueue mQueue;
     private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         textViewResult = findViewById(R.id.text_view_result);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://www.googleapis.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        OpenLibraryApi openLibraryApi = retrofit.create(OpenLibraryApi.class);
+        mQueue = Volley.newRequestQueue(this);
 
-        Call<Post> call = openLibraryApi.getPosts(9780545035170L);
+        jsonParse();
 
-        call.enqueue(new Callback<Post>() {
-            @Override
-            public void onResponse(Call<Post> call, Response<Post> response) {
-                Log.d(TAG, "onResponse: You got a response, and it was Gucci");
 
-                if (!response.isSuccessful()) {
-                    Log.d(TAG, "onResponse:Server was contacted, but could not find what you wanted. ");
-                    textViewResult.setText("Code: " + response.code());
-                    return;
-                }
-
-                try {
-
-                    textViewResult.setText(response.body().getItems().get(0).getVolumeInfo().getTitle());
-                }
-                catch (Exception e) {
-                    textViewResult.setText("Error retrieviing Book name");
-                }
-
-                ImageView imageView = (ImageView) findViewById(R.id.my_image_view);
-
-                try {
-
-                    Glide.with(imageView).load(response.body().getItems().get(0).getVolumeInfo().getImageLinks().getThumbnail()).into(imageView);
-                }
-                catch (Exception e) {
-                    imageView.setImageResource(R.drawable.noimage);
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<Post> call, Throwable t) {
-                textViewResult.setText(t.getMessage());
-            }
-        });
     }
 
+    private void jsonParse() {
+
+        String url = "https://api.myjson.com/bins/kp9wz";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("employees");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject employee = jsonArray.getJSONObject(i);
+
+                                String firstName = employee.getString("firstname");
+                                int age = employee.getInt("age");
+                                String mail = employee.getString("mail");
+
+                                textViewResult.append(firstName + ", " + String.valueOf(age) + ", " + mail + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
 }
